0b5d6e0551b3f2f016ea0cb37ea709564e3add4a
zol22 <zol2295@live.com>
2025-02-24 18:22:36 -0500

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,  // Ensures each route ends with a trailing slash (helps with static exports)
-  //output: 'export',
   images: {
     unoptimized: true,  // Disable image optimization
   }
